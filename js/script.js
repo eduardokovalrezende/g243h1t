06c3903d07716d7a079f1bb684b600a52b482fc8
eduardokovalrezende <110104869+eduardokovalrezende@users.noreply.github.com>
2024-03-11 12:29:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ fazerSorteio = () => {
     document.getElementById("resultado").innerHTML = sorteio;
 
 }
+function escolher_aba(btn){
+    for(let i = 1; i <= 4; i++){
+        let id_btn = "btn"+1;
+        document.getElementById(id_btn).style.backgroundColor = "blue";
+        document.getElementById (id_btn).disabled = false;
+        document.getElementsByClassName(class_aba).style.display= "none";
+    }
+        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn")
+    }
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").disabled = true;
+    }
+
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").disabled = true;
+    }
+
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").disabled = true;
+    }
+
+    
